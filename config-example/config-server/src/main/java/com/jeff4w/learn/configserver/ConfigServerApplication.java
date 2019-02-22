a82c52b278d68c1c39d68d12f319f2a1b9104a95
jeff4wlu <jeff4w@hotmail.com>
2019-02-22 15:40:18 +0800

    aa

--- config-example/config-server/src/main/java/com/jeff4w/learn/configserver/ConfigServerApplication.java
+++ config-example/config-server/src/main/java/com/jeff4w/learn/configserver/ConfigServerApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
 @SpringBootConfiguration
-@EnableDiscoveryClient
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
