8846e3ee6a25610e7fd82dd3b0cc7f41f0dc180b
jeff4wlu <jeff4w@hotmail.com>
2019-02-22 14:48:12 +0800

    aa

---
+++ config-example/config-client/src/main/java/com/jeff4w/learn/configclient/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.jeff4w.learn.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
